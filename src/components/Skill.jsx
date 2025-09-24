a6504835bc9a3aec08adece36fa6736991b75b6a
Hassan Ali <hasu77212.osman@gmail.com>
2025-09-24 19:14:29 +0500

    Update Skill.jsx

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -19,27 +19,6 @@ const Skill = () => {
             
             
             <div className="container mx-auto"> 
-                
-
-{/* <h2
-  className="mb-4 text-center sm:text-left w-full 
-             text-2xl sm:text-3xl md:text-4xl lg:text-5xl 
-             font-extrabold tracking-tight leading-snug max-w-2xl mx-auto sm:mx-0"
->
-  I Use Professional Tools
-</h2>
-
-<p
-  className="text-zinc-400 mb-10 text-center sm:text-left 
-             max-w-2xl mx-auto sm:mx-0 text-base sm:text-lg leading-relaxed"
->
-  I'm Hassan, a skilled developer crafting exceptional websites and mobile apps using modern tools and technologies.
-  From design to deployment, I deliver high-performance digital experiences that stand out.
-</p> */}
-
-
-
-
 
 
 
@@ -54,9 +33,9 @@ const Skill = () => {
 </h2>
 
 <p
-  className=" headline-2 text-zinc-400 mb-10 text-left 
+  className="text-zinc-400 mb-10 text-left 
              max-w-3xl text-base sm:text-lg leading-relaxed 
-             pl-2 sm:pl-4 md:pl-8"
+             pl-2 sm:pl-4 md:pl-8 lg:pl-5"
 >
   I'm Hassan, a skilled developer crafting exceptional websites and mobile apps using modern tools and technologies.
   From design to deployment, I deliver high-performance digital experiences that stand out.
